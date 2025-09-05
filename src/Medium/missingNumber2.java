eff720cd18c76e317576308a4175589c44292e85
Akashgoyal55 <agoyal1@sfsu.edu>
2025-09-04 23:54:03 -0700

    Initial Commit

---
+++ src/Medium/missingNumber2.java
@@ -0,0 +1,36 @@
+package Medium;
+
+// Here we have been given an array, and we have to find all the disappeared numbers in the range of [1,n]
+import java.util.*;
+public class missingNumber2 {
+    public static void main(String[] args) {
+        int[] arr = {4,3,2,7,8,2,3,1};
+        List<Integer> result = findDisappeared(arr);
+        System.out.println(result);
+    }
+    public static List<Integer> findDisappeared(int[] arr){
+        int i=0;
+        while(i<arr.length){
+            int correct = arr[i]-1;
+            if(arr[i]!=arr[correct]){
+                swap(arr, i, correct);
+            }else{
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+
+        for(int index=0; index<arr.length; index++){
+            if(arr[index]!=index+1){
+                ans.add(index+1);
+            }
+        }
+        return ans;
+    }
+    // function to swap the values
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
